175e890a472734a260a09c7bcb93c0f36a466bd9
Abhishek Surse <abhisheksurse5121999.com>
2024-07-28 00:12:31 +0530

    Created todo functionality.

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import Todolist from "./components/Todolist";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Smart ToDo</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* todo list */}
+      <Todolist todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
